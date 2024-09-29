a7f0984a4afc6e78332659fa0c4ee85a167d2e39
Alan Oliveira <alanjhone@gmail.com>
2024-09-29 13:01:17 -0300

    feat: setup initial spring

---
+++ src/test/java/br/com/sicredi/vote/DesafioPautaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.vote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPautaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
